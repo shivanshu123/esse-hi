eac186ff3ec06e5a1ceeca02cacd05d6661fb4ba
Shivanshu Vyas <2016ucp1121@mnit.ac.in>
2020-06-05 17:30:06 +0530

    With console input

--- sum.java
+++ sum.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 class sum
 {
-	public static void main()
+	public static void main(String []arg)
 	{
-		int a=2;
-		int b=3;
+		int a;
+		int b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
 		int sum=a+b;
 		System.out.println("sum "+sum);
 	}
