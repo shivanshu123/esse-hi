9ad479e525d880987b9a635e53f26e61a6bec732
Shivanshu Vyas <2016ucp1121@mnit.ac.in>
2020-06-06 13:47:26 +0530

    Substraction Program

---
+++ substration.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class sum
+{
+	public static void main(String []arg)
+	{
+		int a;
+		int b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int sum=a-b;
+		System.out.println("substration "+sum);
+	}
+}
